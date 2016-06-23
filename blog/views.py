b98dcf5d90ede0ec1dcba49b66cf6810d0fe8198
nbn.bhattarai <070bct522@ioe.edu.np>
2016-06-24 00:58:39 +0545

    search implemented without ordering

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from .models import Post
+
 
 # Create your views here.
 
 def posts_list (request):
     post_data_list = Post.objects.all()
 
-    paginator = Paginator (post_data_list, 5)
+    search_query = request.GET.get('q')
+    if search_query:
+        post_data_list = post_data_list.filter(Q(title__icontains=search_query) |
+            Q(content__icontains=search_query)).distinct()
+
+    paginator = Paginator (post_data_list, 1)
     page_request_var = 'page'
     page_no = request.GET.get(page_request_var)
+
     try:
         data_list = paginator.page(page_no)
     except PageNotAnInteger:
@@ -21,6 +29,7 @@ def posts_list (request):
     context = {
         'page_title':'blog',
         'data_list':data_list,
+        'page_request_var':page_request_var,
     }
 
     return render(request, 'posts_list.html', context)
