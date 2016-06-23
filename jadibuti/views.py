ba0cb257ee5c335217ffe8b793b87de1788d3e54
nbn.bhattarai <070bct522@ioe.edu.np>
2016-06-23 12:48:53 +0545

    commit 1

--- jadibuti/views.py
+++ jadibuti/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view (request):
+    context = {
+        'page_title':'Home'
+        
+    }
+    return render (request, 'home_page.html',context)
