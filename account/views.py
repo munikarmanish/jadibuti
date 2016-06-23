f9ca326975ba5b3786175bee64de66f2cf92be18
Manish Munikar <munikarmanish@gmail.com>
2016-06-23 18:34:20 +0545

    messages part done

--- account/views.py
+++ account/views.py
@@ -7,21 +7,37 @@ from django.contrib import messages
 
 def login_view(request):
     login_form = LoginForm(request.POST or None)
-    if login_form.is_valid():
-        username = login_form.cleaned_data.get('username')
-        password = login_form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        if not user or not user.is_active:
-            messages.error(request, 'Invalid username and/or password.', extra_tags='danger')
-
-        login(request, user)
-        return redirect('home')
-    context = {
-        'login_form': login_form,
-    }
-    return render(request, 'login_form.html', context)
-
+    if request.method == "POST":
+        if login_form.is_valid():
+            # Form valid
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # username and password match
+                if user.is_active:
+                    # user is active
+                    login(request, user)
+                    messages.success(request, "Successfully logged in!")
+                    return redirect('home')
+                else:
+                    # user is disables
+                    messages.error(request, "User is disabled!")
+                    return redirect('login')
+            else:
+                # username/password mismatch
+                messages.error(request, "Invalid username and/or password!")
+                return redirect('login')
+        else:
+            # Form invalid
+            messages.error(request, "Invalid username and/or password!")
+            return redirect('login')
+    else:
+        return render(request, 'login_form.html', {
+            'login_form': login_form,
+        })
 
 def logout_view(request):
     logout(request)
+    messages.success(request, "Successfully logged out!")
     return redirect('home')
--- jadibuti/views.py
+++ jadibuti/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 # Create your views here.
 
 def home_view (request):
-    context = {
-        'page_title':'Home'
-    }
-    return render (request, 'home_page.html',context)
+    return render (request, 'home_page.html', {})
