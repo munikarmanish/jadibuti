74060aa480a8dbcae1caf6e916e54ecb00139036
nbn.bhattarai <070bct522@ioe.edu.np>
2016-06-25 01:50:39 +0545

    home screen sliding carousel added

--- browse/models.py
+++ browse/models.py
@@ -26,3 +26,22 @@ class Herb(models.Model):
     def get_absolute_url (self):
         #return '#'
     	return reverse('browse:herb', kwargs={'slug': self.id})
+
+
+class Disease(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class CarouselImage (models.Model):
+    show = models.BooleanField (null=False, default=True)
+
+    image = models.ImageField (null=False, blank=False, 
+        width_field='width_field', height_field='height_field')
+
+    height_field = models.IntegerField (default=0)
+    width_field = models.IntegerField (default=0)
+
+    caption = models.CharField (max_length=100)
+
+    description = models.CharField (max_length=255)
+
